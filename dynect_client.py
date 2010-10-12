76a305013cc8716993ad2efd225cb6e04d4c8495
Zachary Goldberg <zgs@force.stwing.upenn.edu>
2010-10-12 17:13:39 -0400

    Basic dynect client.  Not too many features, but the barebones are here.

---
+++ dynect_client.py
@@ -0,0 +1,116 @@
+import simplejson, urllib2, urllib
+
+class DynectDNSClient:
+  def __init__(self, customerName, userName, password, defaultDomain=None):
+    self.customerName = customerName
+    self.userName = userName
+    self.password = password
+    self.defaultDomainName = defaultDomain
+    self.sessionToken = None
+
+  def getRecords(self, hostName, type="A", domainName=None):
+    if not domainName:
+      domainName = self.defaultDomainName
+      
+    try:
+      response = self._request('ANYRecord/%s/%s/' % (domainName, hostName), None)
+      return response['data']
+    except urllib2.HTTPError, e:
+      if e.code == 404:
+        return None
+      else:
+        raise e
+
+  def addRecord(self, data, hostName, type="A", TTL=3600, domainName=None):    
+    url, fieldName = self._api_details(type)
+      
+    if not domainName:
+      domainName = self.defaultDomainName
+
+    url = "%s/%s/%s/" % (url, domainName, hostName)
+    data = {"ttl": str(TTL),
+            "rdata": { fieldName: data }}
+
+    response = self._request(url, data)
+    if response['status'] != 'success':
+      return False
+    
+    response = self._publish(domainName)
+    return True
+
+  def deleteRecord(self, data, hostName, type="A", domainName=None):
+    if not domainName:
+      domainName = self.defaultDomainName
+
+    data = self.getRecords(hostName, type, domainName)
+    if not data:
+      return False
+
+    url = data[0]
+    url = url.replace("/REST/", "")
+    try:
+      self._request(url, None, "DELETE")
+      self._publish(domainName)
+    except:
+      return False
+
+    return True
+
+  def _api_details(self, type):
+    if type == "A":
+      return ("ARecord", "address")
+    else:
+      return ("CNameRecord", "cname")
+
+
+
+  def _publish(self, domainName=None):
+    self._request("Zone/%s" % domainName, {"publish": True}, type="PUT")
+
+  def _login(self):
+    response = self._request("Session/", {'customer_name': self.customerName,
+                                                'user_name': self.userName,
+                                                'password': self.password})
+    if response['status'] != 'success':
+      return
+    self.sessionToken = response['data']['token']
+
+  def _request(self, url, post, type=None):
+    fullurl = "https://api2.dynect.net/REST/%s" % url
+
+    if post:
+      postdata = simplejson.dumps(post)
+      req = MethodRequest(fullurl, postdata)
+    else:
+      req = MethodRequest(fullurl)
+      
+    req.add_header('Content-Type', 'application/json')
+    req.add_header('Auth-Token', self.sessionToken)
+    if type:
+      setattr(req, "method", type)
+
+    try:
+      resp = urllib2.urlopen(req)
+      if type:
+        return resp
+      else:
+        return simplejson.loads(resp.read())
+
+    except urllib2.HTTPError, e:
+      if e.code == 400:
+        self._login()
+        return self._request(url, post)
+      else:
+        raise e
+
+class MethodRequest(urllib2.Request):
+  def __init__(self, *args, **kwargs):
+    urllib2.Request.__init__(self, *args, **kwargs)
+    self.method = None
+
+  def get_method(self):
+    if self.method:
+      return self.method
+    return urllib2.Request.get_method(self)
+      
+
